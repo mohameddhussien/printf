e14717153a5545d65f4998e1e7eff4dbf763d52f
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-25 14:49:26 +0000

    Task 0

---
+++ functions.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * print_char - Function to treat next argument
+ * as a charcter.
+ * @arg: arguments.
+ *
+ * Return: 0 - 1.
+ */
+
+int print_char(va_list arg)
+{
+	char c = va_arg(arg, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * _strlen - Function to reset a number to 98.
+ * @s: Address parameter.
+ *
+ * Return: string length.
+*/
+int _strlen(char* s)
+{
+    int i = 0;
+
+    while (*(s + i) != 0)
+    {
+        ++i;
+    }
+    return (i);
+}
+
+/**
+ * print_string - Function to print a string.
+ * @arg: next argument.
+ *
+ * Return: number of characters of the string.
+ */
+
+int print_string(va_list arg)
+{
+    char* str = va_arg(arg, char*);
+
+    if (!str)
+    {
+        str = "(null)";
+        _puts(str);
+        return (0);
+    }
+    _puts(str);
+    return (_strlen(str));
+}
+
+/**
+ * print_percent - Function to print percent.
+ * @arg: argument list.
+ *
+ * Return: 1
+ */
+
+int print_percent(va_list arg)
+{
+    char ch = '%';
+
+    (void)arg;
+    _putchar(ch);
+    return (1);
+}
